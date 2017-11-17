b46e0723d3fd8d5144fb6601b9824a35314b5cd1
Ageu Matheus <ageu_mateus@outlook.com>
2017-11-17 23:48:21 +0100

    Funcao de soma de n matrizes da mesma ordem feita

--- algebrado/models/matriz.py
+++ algebrado/models/matriz.py
@@ -6,6 +6,72 @@
 
 class Mat:
 
-	def __init__(self):
+	# Matrizes
 
-		print('Algbrado')
+	matrizes = []
+
+	def __init__(self, *args):
+
+		# Pegando as matrizes
+		
+		if args:
+			for mat in args:
+				self.matrizes += [mat]
+
+	# Retorna a determinante, Matriz 2x2
+
+	def det2x2(self, A):
+
+		matA = A[0][0] * A[1][1]
+		matB = A[0][1] * A[1][0]
+
+		return matA - matB
+
+	# Soma de n Matrizes, Mesma ordem necessarias
+
+	def soma(self, *args , islist = False):
+
+		# Pegando a quantidade de matrizes 
+
+		if islist:
+			args = args[1]
+			numMats = len(args)
+		else:
+			numMats = len(args)
+
+		if numMats > 0 and self.mesmaOrdem():
+
+			# Fazendo a matriz que sera retornada
+
+			m = len(args[0])    # Numero de linhas
+			n = len(args[0][0]) # numero de colunas
+
+			sMat = [[0]*n]
+			for i in range(m-1):
+				sMat += [[0]*n]
+
+			# Somando as matrizes
+
+			for k in range(numMats):
+				for i in range(m):
+					for j in range(n):
+						sMat[i][j] += args[k][i][j]
+
+			return sMat
+
+		else:
+
+			# Verifica se todas as matrizes sao de mesma ordem
+
+			if self.mesmaOrdem():
+				return self.soma(self, self.matrizes, islist=True)
+			return False
+
+	# Verifica se as matrizes são de mesma ordem
+
+	def mesmaOrdem(self, *args):
+		return True
+
+
+
+
--- main.py
+++ main.py
@@ -2,5 +2,16 @@
 # Algebrado Models
 from algebrado.models.matriz import Mat 
 
-Algebrado = Mat()
+
+A = [ [3,4,2], [3,2,4], [3,2,4] ]
+B = [ [4,4,2], [7,2,4], [3,4,5] ]
+C = [ [6,4,2], [6,0,4], [-32,2,4] ]
+D = [ [2,6,2], [3,2,-4], [5,2,4] ]
+
+
+a = Mat(A,D)
+
+# print(a.soma()) # Soma as matrizes instaciadas
+
+print(a.soma(A,B)) #soma quantas matrizes forem passadas, mesma ordem necessaria
 
